08559f0a8f413d2aa01220d1fab56405480a83a3
BrhmDnsK263 <denysiuk.isi@gmail.com>
2019-10-18 22:45:20 -0300

    busqueda de publicaciones por titulo

--- app_ofertas/views.py
+++ app_ofertas/views.py
@@ -14,3 +14,37 @@ def index(request):
 	categorias = Rubro.objects.all()
 
 	return render(request,'home.html',{'recientes':recientes, 'categorias':categorias})
+
+
+def search(request):
+	
+	# parametro titulo
+	param_titulo = request.GET.get('param_titulo','')
+
+	# parametro order
+
+	#	parametro	efectivo
+	#	parametro	credito
+	#	parametro	debito
+	#	parametro	todas
+	
+	#	parametro delivery
+
+
+	# filtrar titulo
+	publicaciones = Publicacion.objects.filter(titulo__contains=param_titulo)
+
+	#filtrar metodo pago
+
+	#filtrar delivery
+
+	#ordenar
+
+	#agregar las publicaciones  rubros que entre dentro de param titulo
+
+	contexto = {
+		"publicaciones":publicaciones,
+		"param": param_titulo
+	}
+
+	return render(request, "search_results.html", contexto)

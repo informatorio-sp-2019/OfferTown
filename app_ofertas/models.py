aaf2cb937cd2469ec65a898e75a24eb2dbc36aca
simon8x <simonibalo@gmail.com>
2019-10-25 16:17:20 -0300

    cambio en models image field por file field para subir svg a rubros, fix listado grid de ofertas, agregado image.url a grid de rubros

--- app_ofertas/models.py
+++ app_ofertas/models.py
@@ -71,7 +71,7 @@ class Horario(models.Model):
 
 class Rubro(models.Model):
 	nombre = models.CharField(max_length = 25)
-	imagen = models.ImageField(upload_to='fotos_rubros', null=True,blank=True)
+	imagen = models.FileField(upload_to='fotos_rubros', null=True,blank=True)
 
 	def __str__(self):
 		return self.nombre

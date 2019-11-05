c6591c4d37afc4c985f4d472c8b72faa248a942d
BrhmDnsK263 <denysiuk.isi@gmail.com>
2019-11-05 12:43:07 -0300

    fix urls accounts login/logut

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'app_accounts'
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('signup', views.create_user_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.create_user_view, name='signup'),
 ]
 
 

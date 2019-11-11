74d5205c429f47d3c9547c0b335fbf71dcfe9130
BrhmDnsK263 <denysiuk.isi@gmail.com>
2019-11-10 22:30:17 -0300

    config db in heroku

--- _OfferTown/settings.py
+++ _OfferTown/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
